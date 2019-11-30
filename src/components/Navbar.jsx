2af61250e1e602362196fab4c258cee993a6f557
javie-lau <laura.javiera77@gmail.com>
2019-11-30 18:55:12 -0300

    femaestras

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ class Navbar extends Component {
         return(
             <React.Fragment>
                 <nav class="navbar navbar-expand-lg navbar-light" style={shadow}>
-                    <Link className="link"  to="/Home"><a class="navbar-brand" style={styleNav} href="#Home">Femestra</a></Link>
+                    <Link className="link"  to="/Home"><a class="navbar-brand" style={styleNav} href="#Home">Femaestra</a></Link>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="true" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon "></span>
             </button>
